3976c4a48ad7fea5ccdd051c07cba1debfa3aa29
yaseen iqbal <yaseeniqbal119@gmail.com>
2019-09-24 14:48:49 +0500

    1.2

--- db/migrate/20170503091013_create_spree_roles_permission_sets.rb
+++ db/migrate/20170503091013_create_spree_roles_permission_sets.rb
@@ -1,4 +1,4 @@
-class CreateSpreeRolesPermissionSets < ActiveRecord::Migration[4.2]
+class CreateSpreeRolesPermissionSets < ActiveRecord::Migration
   def change
     create_table :spree_roles_permission_sets do |t|
       t.references :role, index: true
